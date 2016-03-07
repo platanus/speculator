05ee15f0140d49b8b739578c5e2c496c9eb82834
Ignacio Baixas <iobaixas@gmail.com>
2016-03-07 17:04:14 -0300

    fix(factories.account): fixes column name

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -5,6 +5,6 @@ FactoryGirl.define do
     base_currency 'BTC'
     quote_currency 'CLP'
     exchange "fake"
-    credentials "base: BTC\nquote: CLP\nbase_balance: 10.0\nquote_balance: 100000.0"
+    encrypted_credentials "base: BTC\nquote: CLP\nbase_balance: 10.0\nquote_balance: 10000000.0"
   end
 end
