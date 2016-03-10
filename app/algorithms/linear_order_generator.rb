796135bb04dbd3caeec54f752620377e0e4ffa72
Ignacio Baixas <iobaixas@gmail.com>
2016-03-10 18:25:30 -0300

    fix(linear_order_generator): fixes volumes beign "floored"

--- app/algorithms/linear_order_generator.rb
+++ app/algorithms/linear_order_generator.rb
@@ -1,8 +1,7 @@
 class LinearOrderGenerator < Struct.new(:instruction, :spread, :step)
-
   def generate(_start_price, _volume)
     buckets = (spread / step).floor
-    volume = (_volume / buckets).floor
+    volume = (_volume / buckets.to_f)
 
     buckets.times.map do |i|
       price = _start_price + step * multiplier * i
@@ -13,5 +12,4 @@ class LinearOrderGenerator < Struct.new(:instruction, :spread, :step)
   def multiplier
     @multiplier ||= (instruction == :bid ? -1 : 1)
   end
-
 end
