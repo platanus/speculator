7dab3cf6023af089d874f02deee69f0c91e94691
Ignacio Baixas <iobaixas@gmail.com>
2016-03-23 10:37:15 -0300

    chore(): renames application

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 Bundler.require(*Rails.groups)
 
-module SurbtcBots
+module Speculator
   class Application < Rails::Application
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
