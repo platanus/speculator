9c75ba46e71d120f1f9fc04605a7446b8d2f4812
Ignacio Baixas <iobaixas@gmail.com>
2016-04-13 17:38:05 -0300

    feat() adds TriggerRobotAlert command

---
+++ app/commands/trigger_robot_alert.rb
@@ -0,0 +1,39 @@
+class TriggerRobotAlert < Command.new(:robot, :title, :message)
+  def perform
+    load_alert
+    if alert_triggered?
+      update_alert
+    else
+      update_alert
+      trigger_alert
+    end
+  end
+
+  private
+
+  attr_reader :alert
+
+  def load_alert
+    @alert = robot.alerts.where(title: title).first
+    @alert = robot.alerts.build title: title if @alert.nil?
+  end
+
+  def alert_triggered?
+    !alert.triggered_at.nil?
+  end
+
+  def update_alert
+    alert.triggered_at = current_time unless alert_triggered?
+    alert.last_triggered_at = current_time
+    alert.message = message || title
+    alert.save!
+  end
+
+  def trigger_alert
+    alert.triggered_at = current_time
+  end
+
+  def current_time
+    @time ||= Time.current
+  end
+end
